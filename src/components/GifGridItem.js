20e62bc3c415fb13a5de98168e9de6156e98c2b5
Jose Angel <jose.tamaroni@idealsur.com>
2022-12-10 11:51:37 -0300

    Se agrega shareGif

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { shareGifs } from '../helpers/shareGifs';
 
 export const GifGridItem = ( { id, title, url } ) => {
 
     return (
-        <div className="card animate__animated animate__fadeIn animate__slower 5s">
+        <div id={id} className="card animate__animated animate__fadeIn animate__slower 5s">
             <img src={url} alt={title} />
-            <p> { title } </p>
+            <p onClick={()=>shareGifs({title:title,url:url,text:'te comparto un gif'})}> { title } </p>
         </div>
     )
 }
---
+++ src/helpers/shareGifs.js
@@ -0,0 +1,18 @@
+// El objeto al menos debe tener una propiedad
+// const objeto = {
+//     title: '',
+//     text:'',
+//     url:'',
+//     files: [file] // puedo compartir más de un archivo
+// }
+export const shareGifs = (objeto) => {
+  if (navigator.share) {
+    navigator
+        .share(objeto)
+        .then(()=> console.log('Successful share'))
+        .catch(err => console.log('Error sharing', err));
+  } else {
+    alert('Does not support sharing')
+    console.log('Does not support sharing')
+  }
+}
